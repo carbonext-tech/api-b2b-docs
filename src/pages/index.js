8ca959b4d0d6237babad8dbd4db3afe3e1b7e6b1
Guilherme Leandro Rolim <guilherme.rolim@carbonext.com.br>
2021-12-14 11:48:55 -0300

    update link button on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/introduction"
+            to="/docs/intro"
           >
             Seu app carbono neutro em 5min ⏱️
           </Link>
