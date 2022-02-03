1c2559b53b58b44f14164732c855e988ef99664a
gui-leandro <guilherme.gl1997@gmail.com>
2022-02-03 17:56:30 -0300

    feat: add experience section

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import styles from "./index.module.css";
 import { Amazon } from "../components/Lotties/Amazon";
 import HomepageFeatures from "../components/HomePageFeatures";
 import { Initialize } from "../components/Initialize";
+import { Experience } from "../components/Experience";
 
 function HomepageHeader() {
   return (
@@ -59,6 +60,7 @@ export default function Home() {
       <main>
         <Initialize />
         <HomepageFeatures />
+        <Experience />
       </main>
     </Layout>
   );
